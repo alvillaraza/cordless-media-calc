7c607f6668199f33b1055ff28377fcf8986e963b
Ray Villaraza <ray.villaraza@strongmind.com>
2024-06-09 09:59:38 -0700

    example js

---
+++ cordless-media-calc/calc.js
@@ -0,0 +1,9 @@
+let btnCompare = document.getElementById('compareLoansButton');
+
+btnCompare.addEventListener('click', function () {
+  console.log('Button clicked');
+
+  console.log(downPayment1.value);
+});
+
+let downPayment1 = document.getElementById('downPayment1');
