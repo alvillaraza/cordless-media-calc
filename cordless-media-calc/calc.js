66f91651e304579852244dec83c33a7b4d59537a
Alexis <alexis@integrate.space>
2024-06-12 10:34:22 -0700

    results displaying

--- cordless-media-calc/calc.js
+++ cordless-media-calc/calc.js
@@ -23,10 +23,9 @@ const loanTerm = (loanTermInput) => parseInt(loanTermInput.value);
 
 let paymentFreq1;
 paymentFrequency1.addEventListener('change', function () {
-    paymentFreq1 = this.value;
+  paymentFreq1 = this.value;
 });
 const numberOfPayments1 = () => {
-
   if (paymentFreq1 == 'Monthly') {
     return loanTerm(loanTerm1) * 12;
   } else if (paymentFreq1 == 'Bi-weekly') {
@@ -40,10 +39,9 @@ const numberOfPayments1 = () => {
 
 let paymentFreq2;
 paymentFrequency2.addEventListener('change', function () {
-    paymentFreq2 = this.value;
+  paymentFreq2 = this.value;
 });
 const numberOfPayments2 = () => {
-
   if (paymentFreq2 == 'Monthly') {
     return loanTerm(loanTerm2) * 12;
   } else if (paymentFreq2 == 'Bi-weekly') {
@@ -57,12 +55,13 @@ const numberOfPayments2 = () => {
 
 btnCompare.addEventListener('click', function () {
   let monthlyPayment1;
-   monthlyPayment1 = calculateMonthlyPayment(
-      principal(loanAmount1),
-      apy(annualInterestRate1),
-      loanTerm(loanTerm1),
-   ),
-     document.getElementById("monthlyPaymentAmountOne").innerHTML = monthlyPayment1;
+  monthlyPayment1 = calculateMonthlyPayment(
+    principal(loanAmount1),
+    apy(annualInterestRate1),
+    loanTerm(loanTerm1),
+  );
+  document.getElementById('monthlyPaymentAmountOne').innerHTML =
+    monthlyPayment1;
   console.log(
     'monthly payment for 1',
     calculateMonthlyPayment(
@@ -71,6 +70,16 @@ btnCompare.addEventListener('click', function () {
       loanTerm(loanTerm1),
     ),
   );
+
+  let monthlyPayment2;
+  monthlyPayment2 = calculateMonthlyPayment(
+    principal(loanAmount2),
+    apy(annualInterestRate2),
+    loanTerm(loanTerm2),
+  );
+  document.getElementById('monthlyPaymentAmountTwo').innerHTML =
+    monthlyPayment2;
+
   console.log(
     'monthly payment for 2',
     calculateMonthlyPayment(
@@ -80,6 +89,15 @@ btnCompare.addEventListener('click', function () {
     ),
   );
 
+  let totalInterestPaid1;
+  totalInterestPaid1 = calculateTotalInterest(
+    principal(loanAmount1),
+    apy(annualInterestRate1),
+    numberOfPayments1(),
+  );
+  document.getElementById('totalInterestPaidOne').innerHTML =
+    totalInterestPaid1;
+
   console.log(
     'total interest for 1',
     calculateTotalInterest(
@@ -88,6 +106,16 @@ btnCompare.addEventListener('click', function () {
       numberOfPayments1(),
     ),
   );
+
+  let totalInterestPaid2;
+  totalInterestPaid2 = calculateTotalInterest(
+    principal(loanAmount2),
+    apy(annualInterestRate2),
+    numberOfPayments2(),
+  );
+  document.getElementById('totalInterestPaidTwo').innerHTML =
+    totalInterestPaid2;
+
   console.log(
     'total interest for 2',
     calculateTotalInterest(
@@ -97,6 +125,14 @@ btnCompare.addEventListener('click', function () {
     ),
   );
 
+  let totalCostOfLoan1;
+  totalCostOfLoan1 = calculateTotalMortgageCost(
+    principal(loanAmount1),
+    apy(annualInterestRate1),
+    loanTerm(loanTerm1),
+  );
+  document.getElementById('totalCostOfLoanOne').innerHTML = totalCostOfLoan1;
+
   console.log(
     'total mortgage cost for 1',
     calculateTotalMortgageCost(
@@ -105,6 +141,15 @@ btnCompare.addEventListener('click', function () {
       loanTerm(loanTerm1),
     ),
   );
+
+  let totalCostOfLoan2;
+  totalCostOfLoan2 = calculateTotalMortgageCost(
+    principal(loanAmount2),
+    apy(annualInterestRate2),
+    loanTerm(loanTerm2),
+  );
+  document.getElementById('totalCostOfLoanTwo').innerHTML = totalCostOfLoan2;
+
   console.log(
     'total mortgage cost for 2',
     calculateTotalMortgageCost(
